a9bf7fdd67fb77d46c61ff60fb9b42a99142a3c2
jflondonog <35697747+jflondonog@users.noreply.github.com>
2018-10-29 10:52:20 -0500

    Add files via upload

---
+++ proyecto/codigo/Station.java
@@ -0,0 +1,55 @@
+
+
+public class Station extends Node{
+
+    int StationType;          //0 = Rápido, 1= Medio, 2= Lento
+    double L;                 // Tiempo de carga en horas para cada tipo de estación y para cada punto de soporte
+    double G;                 // Nivel de la batería en watts/hora para cada tipo de estación y para cada punto de soporte
+
+
+
+
+
+    //Constructor Station
+    public Station (int id, String name, double X, double Y, int sType) {
+        this.idNode = id;
+        this.name = name;
+        this.posX = X;
+        this.posY = Y;
+        this.StationType = sType;
+
+}
+    //Obtener L
+    public double getL() {
+        return L;
+    }
+
+    //Obtener G
+    public double getG() {
+        return G;
+    }
+
+    //Setear L
+    public void setL(double l) {
+        L = l;
+    }
+
+    //Setear G
+    public void setG(double g) {
+        G = g;
+    }
+
+    public int getStationType() {
+        return StationType;
+    }
+
+    public void setStationType(int stype){
+        this.StationType = stype;
+    }
+}
+
+
+
+
+
+
