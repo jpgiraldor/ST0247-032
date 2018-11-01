e3e1e908c9819ecb4d9f9861b2d04209d2848e66
jflondonog <35697747+jflondonog@users.noreply.github.com>
2018-10-31 19:07:04 -0500

    Update Station.java

--- proyecto/codigo/Station.java
+++ proyecto/codigo/Station.java
@@ -4,7 +4,7 @@ public class Station{
 
    int StationType;          //0 = Rápido, 1= Medio, 2= Lento
     double[] L = {0.51, 1.01, 2.04}; // Tiempo de carga en horas para cada tipo de estación 
-    double G;                 // Nivel de la batería en watts/hora para cada tipo de estación y para cada punto de soporte
+    //double G;                 // Nivel de la batería en watts/hora para cada tipo de estación y para cada punto de soporte
     int idNode;
     String name;
     double posX;
